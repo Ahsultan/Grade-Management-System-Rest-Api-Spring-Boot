9f2656bf1715cfe710fc0b2172ca32d57ebb32bc
Ahsultan <petersultan307@gmail.com>
2024-01-25 21:23:04 +0530

    Initial commit

---
+++ src/main/java/com/gradems/grademangementsystem/GradeMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gradems.grademangementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeMangementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradems/grademangementsystem/GradeMangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradems.grademangementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeMangementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
